1d7ba574e80b1d5c4deb2d1a81ef812f83cc95eb
Ty Frendo <TNTLauncherPacks@Gmail.com>
2022-04-10 00:17:37 +0300

    Implemented

--- megalist.cpp
+++ megalist.cpp
@@ -7,7 +7,16 @@ struct MegaNode
     MegaNode *prev;
 
     int remaining;
-    int origin_index; // the index in the procs vector, this is bad practice ikr.
+    int origin_index; // The index for the process in the procs vector, this is bad practice ikr.
+
+    MegaNode(Proc proc, int remaining, int origin_index) : proc(proc)
+    {
+        this->remaining = remaining;
+        this->origin_index = origin_index;
+
+        next = NULL;
+        prev = NULL;
+    }
 };
 
 class MegaList
@@ -29,15 +38,103 @@ public:
         return size;
     }
 
-    void Insert(MegaNode node)      // insert to the left
+    void Insert(MegaNode *node)      // insert to the left
     {
         if (current == NULL)
         {
+            current = front = back = node;
+            
+            return;
+        }
+        
+        node->next = current;
+        node->prev = current->prev;
+        current->prev = node;
+
+        if (node->prev != NULL)
+        {
+            node->prev->next = node;
+        }
+
+        if (back->prev != NULL)
+        {
+            back = back->prev;
+        }
+    }
+
+    // Inserts a MegaNode into a sorted list based on the remaining time
+    void InsertSorted(MegaNode *node)
+    {
+        if (current == NULL)
+        {
+            current = front = back = node;
+
             return;
         }
 
-        
+        MegaNode *cur = back;
+
+        while (cur != NULL)
+        {
+            if (node->remaining <= cur->remaining)
+            {
+                node->next = cur;
+                node->prev = cur->prev;
+                cur->prev = node;
+
+                if (node->prev != NULL)
+                {
+                    node->prev->next = node;
+                }
+
+                break;
+            }
+
+            cur = cur->next;
+        }
+
+        if (cur == NULL)
+        {
+            cur->prev = front;
+            front->next = cur;
+
+            front = front->next;
+        }
+
+        if (back->prev != NULL)
+        {
+            back = back->prev;
+        }
     }
-    void InsertSorted(MegaNode node); // sorted by remaining time
-    void Remove();                // remove current
+
+    // Removes the current MegaNode and assigns a new one based on the value of the argument, defaults to the next one
+    void Remove(bool forward = true)
+    {
+        if (current->next != NULL)
+        {
+            current->next->prev = current->prev;
+        }
+        else
+        {
+            front = current->prev;
+        }
+
+        if (current->prev != NULL)
+        {
+            current->prev->next = current->next;
+        }
+        else
+        {
+            back = current->next;
+        }
+
+        if (forward)
+        {
+            current = current->next;
+        }
+        else
+        {
+            current = current->prev;
+        }
+    }
 };
