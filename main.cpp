ffd8397d9e42d048aa2e9d84544a761ab169ac0c
elmomani <ahmad.mom.20000@gmail.com>
2022-04-10 00:46:17 +0200

    sjf parallel example

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include "sjf_scheduler.cpp"
 
-
-int main(){
-    //very cool main function
+int main()
+{
+    // very cool main function
+    std::cout << "Welcome to Oh-no Simulator, ain't no OS\n";
+    SJFScheduler sjf;
+    sjf.ParallelLoad("in.txt");
 }
