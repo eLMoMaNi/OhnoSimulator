e791b05a6ddd108bb6c5bf1ff01f66e3ce492ca3
Ty Frendo <TNTLauncherPacks@Gmail.com>
2022-04-10 16:23:24 +0300

    Implemented the function

--- fifo_scheduler.cpp
+++ fifo_scheduler.cpp
@@ -2,5 +2,49 @@
 #include <algorithm>
 class FIFOScheduler : public Scheduler
 {
-    
+    MegaNode *current = NULL;
+    Proc Dispatch(std::vector<MegaNode *> newcomers, int time)
+    {
+        for (int i = 0; i < newcomers.size(); ++i)
+        {
+            mega_list.InsertBack(newcomers[i]);
+        }
+
+        if (current == NULL)
+        {
+            if (mega_list.Size() == 0)
+            {
+                return IDLE_PROC;
+            }
+            else
+            {
+                current = mega_list.GetCurrent();
+                procs[current->origin_index].start_time = time;
+            }
+        }
+
+        if (current->remaining > 0)
+        {
+
+            --(current->remaining);
+            return current->proc;
+        }
+
+        procs[current->origin_index].finish_time = time;
+
+        mega_list.Remove(false);
+        delete current;
+
+        if (mega_list.Size() == 0)
+        {
+            current = NULL;
+            return IDLE_PROC;
+        }
+
+        current = mega_list.GetCurrent();
+        procs[current->origin_index].start_time = time;
+        --(current->remaining);
+        
+        return current->proc;
+    }
 };
