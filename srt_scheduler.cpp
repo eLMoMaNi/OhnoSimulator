a4c8521980674845d55911f5299b52f5239d4fee
Ty Frendo <TNTLauncherPacks@Gmail.com>
2022-04-11 21:12:16 +0300

    Implemented the scheduler

---
+++ srt_scheduler.cpp
@@ -0,0 +1,69 @@
+#include "Headers/scheduler.h"
+
+class SRTScheduler : public Scheduler
+{
+    MegaNode *current = NULL;
+    Proc Dispatch(std::vector<MegaNode *> newcomers, int time)
+    {
+        for (int i = 0; i < newcomers.size(); ++i)
+        {
+            mega_list.InsertSorted(newcomers[i]);
+        }
+
+        if (current == NULL)
+        {
+            if (mega_list.Size() == 0)
+            {
+                return IDLE_PROC;
+            }
+            else
+            {
+                current = mega_list.GetCurrent();
+                procs[current->origin_index].start_time = time;
+
+                fragments.push_back(current->proc.proc_name);
+            }
+        }
+
+        if (current != mega_list.GetCurrent())
+        {
+            current = mega_list.GetCurrent();
+            if (procs[current->origin_index].start_time == -1)
+            {
+                procs[current->origin_index].start_time = time;
+            }
+
+            fragments.push_back(current->proc.proc_name);
+        }
+
+        if (current->remaining > 0)
+        {
+
+            --(current->remaining);
+            return current->proc;
+        }
+
+        procs[current->origin_index].finish_time = time;
+
+        mega_list.Remove(false);
+        delete current;
+
+        if (mega_list.Size() == 0)
+        {
+            current = NULL;
+            return IDLE_PROC;
+        }
+
+        current = mega_list.GetCurrent();
+        if (procs[current->origin_index].start_time == -1)
+        {
+            procs[current->origin_index].start_time = time;
+        }
+
+        fragments.push_back(current->proc.proc_name);
+
+        --(current->remaining);
+
+        return current->proc;
+    }
+};
