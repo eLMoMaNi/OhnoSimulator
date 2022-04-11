bb27d9b3f97ac21c08be03b042eb9aea1382f8ad
Ty Frendo <TNTLauncherPacks@Gmail.com>
2022-04-11 16:36:42 +0300

    Removed unnecessary functions

--- fifo_scheduler.cpp
+++ fifo_scheduler.cpp
@@ -1,5 +1,5 @@
 #include "Headers/scheduler.h"
-#include <algorithm>
+
 class FIFOScheduler : public Scheduler
 {
     MegaNode *current = NULL;
@@ -47,63 +47,4 @@ class FIFOScheduler : public Scheduler
         
         return current->proc;
     }
-
-    static bool CompByFinish(const Proc &l, const Proc &r)
-    {
-        return l.finish_time < r.finish_time;
-    }
-
-    static bool CompByArrive(const Proc &l, const Proc &r)
-    {
-        return l.arrive_time < r.arrive_time;
-    }
-
-public:
-    void ParallelLoad(std::string filename)
-    {
-        std::ifstream file(filename);
-
-        int n;
-        file >> n;
-
-        for (int i = 0; i < n; ++i)
-        {
-            std::string proc_name;
-            int arrive_time, processing_time;
-
-            file >> proc_name >> arrive_time >> processing_time;
-
-            Proc proc(proc_name, processing_time, arrive_time);
-            procs.push_back(proc);
-        }
-
-        std::sort(procs.begin(), procs.end(), CompByArrive);
-
-        int arrive_idx = 0, time = 0;
-        while (true)
-        {
-            std::vector<MegaNode *> newcomers;
-            while (procs[arrive_idx].arrive_time <= time && arrive_idx < procs.size())
-            {
-                MegaNode *proc_node = new MegaNode(procs[arrive_idx], procs[arrive_idx].processing_time, arrive_idx);
-                newcomers.push_back(proc_node);
-
-                ++arrive_idx;
-            }
-
-            Proc dispatched = Dispatch(newcomers, time);
-
-            if (dispatched == IDLE_PROC && arrive_idx > procs.size() - 1)
-            {
-                std::cout << "\nFinished all processes!\n" << std::endl;
-                
-                break;
-            }
-
-            std::cout << "At time: " << time << "\t process " << dispatched.proc_name << " is working\n";
-
-            ++time;
-        }
-    }
-
 };
--- sjf_scheduler.cpp
+++ sjf_scheduler.cpp
@@ -1,5 +1,5 @@
 #include "Headers/scheduler.h"
-#include <algorithm>
+
 class SJFScheduler : public Scheduler
 {
     MegaNode *current = NULL;
@@ -46,75 +46,5 @@ class SJFScheduler : public Scheduler
         procs[current->origin_index].start_time = time;
         --(current->remaining);
         return current->proc;
-    }
-    static bool CompByFinish(const Proc &l, const Proc &r)
-    {
-        return l.finish_time < r.finish_time;
-    }
-    static bool CompByArrive(const Proc &l, const Proc &r)
-    {
-        return l.arrive_time < r.arrive_time;
-    }
-
-public:
-    void ParallelLoad(std::string filename)
-    {
-
-        // for time in N:
-        // proc=dispatch(newcomers)
-        //  print(for time:{time} the process {proc} is working)
-
-        // load processes from a file parallelly
-        std::ifstream file(filename);
-        int n;
-        file >> n;
-        for (int i = 0; i < n; i++)
-        {
-            std::string proc_name;
-            int arrive_time, processing_time;
-            file >> proc_name >> arrive_time >> processing_time;
-            Proc proc(proc_name, processing_time, arrive_time);
-            procs.push_back(proc);
-        }
-        std::sort(procs.begin(), procs.end(), CompByArrive);
-        int arrive_idx = 0; // index to procs to flow by arrive time
-        int time = 0;
-        while (true)
-        {
-            // get newcomers
-            std::vector<MegaNode *> newcomers;
-            while (procs[arrive_idx].arrive_time <= time && arrive_idx < procs.size())
-            {
-                MegaNode *proc_node = new MegaNode(procs[arrive_idx], procs[arrive_idx].processing_time, arrive_idx);
-                newcomers.push_back(proc_node);
-                ++arrive_idx;
-            }
-            Proc dispatched = Dispatch(newcomers, time);
-
-            if (dispatched == IDLE_PROC && arrive_idx > procs.size() - 1)
-            {
-                std::cout << "\nFinished all processes!\n" << std::endl;
-                break;
-            }
-            std::cout << "At time: " << time << "\t process " << dispatched.proc_name << " is working\n";
-            ++time;
-        }
-    }
-
-    void PrintStatistics()
-    {
-        std::vector<Proc> tmp_vec = procs;
-        std::sort(tmp_vec.begin(), tmp_vec.end(), CompByFinish);
-        std::cout << "Processes Order: ";
-        for (int i = 0; i < tmp_vec.size(); i++)
-        {
-            Proc proc = tmp_vec[i];
-            std::cout << proc.proc_name;
-        }
-        for (int i = 0; i < procs.size(); i++)
-        {
-            Proc proc = procs[i];
-            std::cout << "\nProcess: " << proc.proc_name << "\t Response: " << proc.GetResponseTime() << "\t TA: " << proc.GetTimearound() << "\t Delay: " << proc.GetDelay();
-        }
     }
 };
