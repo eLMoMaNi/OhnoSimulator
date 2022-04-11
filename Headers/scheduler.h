5c7bb5333a24e39539e5ade1a17608f819f7e9ba
elmomani <ahmad.mom.20000@gmail.com>
2022-04-11 22:32:37 +0200

    add interactive mode

--- Headers/scheduler.h
+++ Headers/scheduler.h
@@ -25,7 +25,8 @@ public:
     static bool CompByArrive(const Proc &l, const Proc &r);
 
     void ParallelLoad(std::string filename);
-    void Simulate();
+    void Simulate(bool interactive = false,int time=0);
+    void InteractiveLoad();
     void PrintStatistics();
     void ExitProcess();
 };
