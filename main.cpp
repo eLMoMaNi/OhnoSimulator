91f3adefe249b5f593581190ec946f9fa07b0074
elmomani <ahmad.mom.20000@gmail.com>
2022-04-13 11:16:43 +0300

    example of example_gen

--- main.cpp
+++ main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include "sjf_scheduler.cpp"
 #include "rr_scheduler.cpp"
+#include "example_generator.h"
+
+
+
+
 int main()
 {
     // very cool main function
     std::cout << "Welcome to Oh-no Simulator, ain't no OS\n\n";
     SJFScheduler sjf;
+    int NUMBER_OF_FILES=10;
+    for (int i=0;i<NUMBER_OF_FILES;i++)
+    {
+    //generate random processes file
+    GenerateFile(2);
     sjf.ParallelLoad("in.txt");
     sjf.Simulate();
     sjf.PrintStatistics();
+    }
+
     // Example on using RR scheduler
     //RRScheduler rr;
     //rr.ParallelLoad("in.txt");
